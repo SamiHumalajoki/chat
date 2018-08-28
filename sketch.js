adfb4e582cc4e3bcf42ca7c63d37c9a468d7a0e8
SamiHumalajoki <42648047+SamiHumalajoki@users.noreply.github.com>
2018-08-28 21:27:59 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,4 +2,18 @@ function setup() {
           var cnv = createCanvas(1000, 800);
           frameRate(20);
           background(51);
-        }
+}
+
+$(function () {
+        
+    var socket = io.connect('https://samis-chat.herokuapp.com');
+    $('form').submit(function(){
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+});
